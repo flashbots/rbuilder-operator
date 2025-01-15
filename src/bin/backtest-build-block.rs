4a4ec7d17945bdefbdfb60385c2fb65e9628d3c6
Ryan Schneider <ryanleeschneider@gmail.com>
2025-01-15 15:58:08 -0800

    chore(deps): Update to rbuilder c1c5595

--- src/bin/backtest-build-block.rs
+++
@@ -1,7 +0,0 @@
-use rbuilder::backtest::run_backtest_build_block;
-use rbuilder_operator::flashbots_config::FlashbotsConfig;
-
-#[tokio::main]
-async fn main() -> eyre::Result<()> {
-    run_backtest_build_block::<FlashbotsConfig>().await
-}
